02b927cda9303a3796214dc6ae729d7f84b0e964
kau13543 <133983761+kau13543@users.noreply.github.com>
2023-05-25 09:59:37 -0400

    Update Week2.java

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -15,6 +15,7 @@ public class Week2 {
      */
     public static void main(String[] args) {
         System.out.println("this is my first git operation");
+        System.out.println("this is my first git operation");
         // TODO code application logic here
     }
     
